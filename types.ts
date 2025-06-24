e150f4d54bcc1033f172089ee75426e6136fe8b3
naa-siuuuu-ff <nasifhasan2005@gmail.com>
2025-06-24 08:06:58 +0800

    posting to database is half complete

---
+++ types.ts
@@ -0,0 +1,10 @@
+interface Buffet {
+    $id: string
+    $createdAt: string
+    clearedby: Date
+    leftover: number
+    additionaldetails: string
+    level: number
+    locationdetails: string
+}
+
